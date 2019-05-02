a00612538d6f7ede106f3ef5ab612b461cd9f563
Becky <you@example.com>
2019-05-02 13:13:05 -0400

    admin login

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../services/admin.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  loggedIn: boolean = false;
 
-  constructor() { }
+  constructor(private adminService: AdminService) { }
 
   ngOnInit() {
+    console.log(this.loggedIn);
+  }
+
+  loginToggle() {
+     this.loggedIn = !this.loggedIn;
+    console.log(this.loggedIn);
+  }
+
+  login(email: string, password: string){
+    //console.log(email, password);
+    this.adminService.login(email, password).subscribe(
+      data => {
+        console.log(data);
+      }
+    )
+    this.loggedIn = false;
   }
 
 }
